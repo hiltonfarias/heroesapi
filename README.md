954a4c725ee41485132f881bace5a15f64833c42
hiltonfarias <hilton.farias.da.silva@gmail.com>
2021-04-22 19:28:31 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Heroes Storage API with WebFlux
+
+### We started by generating a Scaffolding with Spring initializr containing the following dependencies:
+- Lombok
+- Spring Reactive Web
+
+### 
+- Spring doc
+- DynamoDB
+- AWS CLI
---
+++ src/test/java/com/hiltonfarias/heroesapi/HeroesapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiltonfarias.heroesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
